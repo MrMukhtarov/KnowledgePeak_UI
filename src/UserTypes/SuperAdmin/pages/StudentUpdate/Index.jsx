ba7fa123a49d6f0fa3242c1b869bd70fc62e51c8
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-11-03 21:00:23 +0400

    Udpate Student for admin

--- src/UserTypes/SuperAdmin/pages/StudentUpdate/Index.jsx
+++ src/UserTypes/SuperAdmin/pages/StudentUpdate/Index.jsx
@@ -1,9 +1,314 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const Index = () => {
-  return (
-    <div>Index</div>
-  )
-}
+  const navigate = useNavigate();
+  const { username } = useParams();
+  const [student, setStudent] = useState({});
+  const [inputs, setInputs] = useState({});
+  const [errorMessages, setErrorMessages] = useState({});
+  const [error, setError] = useState("");
+  const [selectGender, setSelectGender] = useState("");
+  const [selectStatus, setSelectStatus] = useState("");
+  const user = JSON.parse(localStorage.getItem("user"));
 
-export default Index
+  useEffect(() => {
+    axios
+      .get(`https://localhost:7153/api/StudentAuth/GetByUserName?userName=${username}`, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${user.token}`,
+        },
+      })
+      .then((res) => {
+        setStudent(res.data);
+        setInputs(res.data);
+        setSelectStatus(res.data.status);
+      });
+  }, []);
+
+  console.log(inputs);
+  const handleInputChange = (e) => {
+    const { name, value, files, type } = e.target;
+
+    setInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    if (name === "gender") {
+      setSelectGender(value);
+    }
+    if (name === "status") {
+      setSelectStatus(value);
+    }
+    if (type === "file" && files != null && files.length > 0) {
+      const selectedFile = files[0];
+
+      setInputs((prevInputs) => ({
+        ...prevInputs,
+        [name]: selectedFile,
+      }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formdata = new FormData();
+    formdata.append("name", inputs.name);
+    formdata.append("surName", inputs.surName);
+    formdata.append("imageFile", inputs.imageFile);
+    formdata.append("age", inputs.age);
+    formdata.append("gender", inputs.gender);
+    formdata.append("email", inputs.email);
+    formdata.append("status", inputs.status);
+
+    axios
+      .put(
+        `https://localhost:7153/api/StudentAuth/UpdateProfileAdmin?userName=${username}`,
+        formdata,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          navigate("/superadmin/student");
+        }
+      })
+      .catch((e) => {
+        if (e.response && e.response.data && e.response.data.errors) {
+          setErrorMessages(e.response.data.errors);
+        } else {
+          setError(e.response.data.message);
+        }
+      });
+  };
+  return (
+    <section className="faculty_create py-4">
+      <div className="container">
+        <div className="d-flex align-items-center gap-5">
+          <i
+            onClick={() => navigate(-1)}
+            style={{ cursor: "pointer" }}
+            className="fa-solid fa-arrow-left"
+          ></i>
+          <h5>Student Update</h5>
+        </div>
+        <form className="w-50 m-auto mt-5" onSubmit={(e) => handleSubmit(e)}>
+          <div className="error-messages">
+            <p style={{ color: "red" }} className="error-message">
+              {error && error.includes("Exist") ? error : ""}
+            </p>
+          </div>
+          {/* ---- */}
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Enter Name"
+              onChange={handleInputChange}
+              name="name"
+              value={inputs.name}
+            />
+            {errorMessages.Name ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.Name}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("Name") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <div className="form-group mt-3">
+            <label className="mb-1" htmlFor="short">
+              Surname
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="short"
+              placeholder="Enter Surname"
+              onChange={handleInputChange}
+              name="surName"
+              value={inputs.surName}
+            />
+            {errorMessages.Surname ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.Surname}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("Surname") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <div className="form-group">
+            <label htmlFor="ahe">Age</label>
+            <input
+              type="text"
+              className="form-control"
+              id="ahe"
+              placeholder="Enter Age"
+              onChange={handleInputChange}
+              name="age"
+              defaultValue={student.age}
+            />
+            {errorMessages.Age ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.Age}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("Age") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <img width={40} src={student.imageUrl} alt="" />
+          <div className="form-group">
+            <label htmlFor="img">Image</label>
+            <input
+              type="file"
+              className="form-control"
+              id="img"
+              placeholder="Enter Image"
+              onChange={handleInputChange}
+              name="imageFile"
+              value={student.imageFile}
+            />
+            {errorMessages.ImageFile ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.ImageFile}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("ImageFile") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <div className="form-group">
+            Current Gender :{" "}
+            {student.gender === 1 ? (
+              <span style={{ color: "green" }}>Male</span>
+            ) : (
+              <span style={{ color: "green" }}>Female</span>
+            )}
+            <br />
+            <label htmlFor="gender">Gender</label>
+            <select
+              type="text"
+              className="form-control"
+              id="gender"
+              placeholder="Enter Gender"
+              onChange={handleInputChange}
+              name="gender"
+              value={selectGender}
+            >
+              <option value="1">Male</option>
+              <option value="2">Female</option>
+            </select>
+            {errorMessages.Gender ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.Gender}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("gender") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <div className="form-group">
+            <label htmlFor="eml">Email</label>
+            <input
+              type="text"
+              className="form-control"
+              id="eml"
+              placeholder="Enter Email"
+              onChange={handleInputChange}
+              name="email"
+              defaultValue={student.email}
+            />
+            {errorMessages.Email ? (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {errorMessages.Email}
+                </p>
+              </div>
+            ) : (
+              <div className="error-messages">
+                <p style={{ color: "red" }} className="error-message">
+                  {error && error.includes("Email") ? error : ""}
+                </p>
+              </div>
+            )}
+          </div>
+          {/* ---- */}
+          <div className="form-group">
+            Current Status :{" "}
+            {student.status === 7 ? (
+              <span style={{ color: "green" }}>Student</span>
+            ) : (
+              <span style={{ color: "green" }}>Kicked Out</span>
+            )}
+            <select
+              type="text"
+              className="form-control"
+              id="status"
+              placeholder="Change Status"
+              onChange={handleInputChange}
+              name="status"
+              value={selectStatus}
+            >
+              <option value="" selected disabled>
+                Select Status
+              </option>
+              <option value="7">Student</option>
+              <option value="9">Kicked Out</option>
+            </select>
+          </div>
+          {/* ---- */}
+          <button
+            style={{ backgroundColor: "#002140" }}
+            type="submit"
+            className="btn btn-success mt-2"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Index;
